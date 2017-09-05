090aa3eb4bd9113bfcdeac4ed9e0f4ad98052506
st970703 <st970703@gmail.com>
2017-09-05 18:59:28 +1200

    q2

--- A2_Q2.asm
+++ A2_Q2.asm
@@ -1,2 +1,53 @@
-# Assignment 02- Q2 (a)
+# Assignment 02- Q2
+# (a)
 # ARM 
+# (i)
+# load base address of table
+ori $t0, $0, 1024
+# initialize loop counter
+ori $t1, $0, 200
+# clear r3 (using r3 XOR r3)
+xor $t3, $t3, $t3
+
+L1:
+# get first addition operand
+lw $t4, 0($t0)
+# add to r2
+add $t3, $t3, $t4
+# increment to next table element
+addi $t0, $t0, 4
+# decrement loop counter
+addi $t1, $t1, -1
+# if loop counter is not 0, go to L1
+bne $0, $t1, L1
+
+(ii)
+# r1 = r23:0 concatenated with r231:4
+# Assume $t1 and $t2
+# $t3 and $t4 are temporaty registers
+# shift left by 7 bytes (rotate)
+sll $t3, $t2, 28
+# shift right by 1 bytes (shift)
+srl $t4, $t2, 4
+# combine $t4 and $t3
+or $t1, $t3, $t4
+
+# (b)
+Average instruction execution time = Clock period * Average CPI
+(i)
+How much faster (ARM/MIPS)
+= (ARM CPU performance)/(MIPS CPU Performance)
+= (MIPS -> Instruction count*CPI*Clock cycle time)/(ARM -> Instruction count*CPI*Clock cycle time)
+= (8*CPI*(1/1.5))/(8*CPI) = 0.67
+
+The ARM CPU perfomance is 0.33 times slower.
+
+(ii)
+How much faster (ARM/MIPS)
+= (ARM CPU performance)/(MIPS CPU Performance)
+= (MIPS -> Instruction count*CPI*Clock cycle time)/(ARM -> Instruction count*CPI*Clock cycle time)
+= (3*CPI*(1/1.5))/(1*CPI) = 2
+
+The ARM CPU perfomance is 2 times faster.
+
+
